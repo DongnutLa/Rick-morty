d8619d5794c7c8da25d01e2581957763cbb94612
DongnutLa <luahernandezdu@unal.edu.co>
2021-12-20 19:34:59 -0500

    [FIX] Home page header

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/">
+                    <a href="/Rick-morty">
                         Rick & Morty API consumption
                     </a>
                 </h1>
